59ef64a5acc8d3d9bc6a643719295077dc62d922
Wahid Bawa <wbawa@riis.com>
2023-03-21 23:09:49 -0400

    Added build argument missing from ubuntu generate script

--- dji-flightrecord-kit/build/Ubuntu/FRSample/generate.sh
+++ dji-flightrecord-kit/build/Ubuntu/FRSample/generate.sh
@@ -3,6 +3,6 @@ executableSourceFolder="../../../source/FRSample"
 
 echo "Which do you want to build? Please Input the number: "
 
-cmake -D platform=${platform} -DENABLE_SHARED_LIB=false ${executableSourceFolder}
+cmake -D platform=${platform} -DENABLE_SHARED_LIB=false -DCMAKE_BUILD_TYPE=Release ${executableSourceFolder}
 make -j$(nproc)
 
--- dji-flightrecord-kit/build/Ubuntu/FlightRecordStandardizationCpp/generate.sh
+++ dji-flightrecord-kit/build/Ubuntu/FlightRecordStandardizationCpp/generate.sh
@@ -3,5 +3,5 @@ sourceFolder="../../../source/FlightRecordStandardizationCpp"
 
 echo "Which do you want to build? Please Input the number: "
 
-cmake -D platform=${platform} -DENABLE_SHARED_LIB=false ${sourceFolder}
+cmake -D platform=${platform} -DENABLE_SHARED_LIB=false -DCMAKE_BUILD_TYPE=Release ${sourceFolder}
 make -j$(nproc)
